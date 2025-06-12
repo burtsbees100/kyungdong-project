fe70e0de4375b50d3598849556fdb9a91797a049
burtsbees100 <kje_4227@naver.com>
2025-06-12 21:04:55 +0900

    4일차

--- js/common.js
+++ js/common.js
@@ -3,6 +3,8 @@ const getAll = (target) => document.querySelectorAll(target);
 
 const preventDefaultAnchor = () => {
     const $links = getAll('a[href="#"]');
+    $links.forEach((link) => link.addEventListener('click', (e) => e.preventDefault()));
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 };
 
 const navi = () => {
@@ -10,7 +12,57 @@ const navi = () => {
 };
 const familySite = () => {};
 
-const comInit = () => {};
+const siteMap = () => {
+    const $bg = get('.bg');
+    const $sitemap = get('.sitemap');
+    const $allmenu = get('#header .util .all-menu');
+    const $close = get('.sitemap .close');
+
+    $allmenu.addEventListener('click', (e) => {
+        $bg.classList.add('on');
+        $sitemap.classList.add('on');
+    });
+
+    $close.addEventListener('click', (e) => {
+        $bg.classList.remove('on');
+        $sitemap.classList.remove('on');
+    });
+};
+
+const topBar = () => {
+    const $top = get('.top');
+    const $header = get('#header');
+
+    let ty = 0;
+    window.addEventListener('scroll', (e) => {
+        //window.scrollY , window.pageYOffset
+        // console.log(window.scrollY);
+        ty = window.scrollY;
+        if (ty > 300) {
+            //top에 on 붙이기
+            $top.classList.add('on');
+        } else {
+            $top.classList.remove('on');
+        }
+
+        if (ty > 80) {
+            //top에 on 붙이기
+            $header.classList.add('on');
+        } else {
+            $header.classList.remove('on');
+        }
+    });
+
+    $top.addEventListener('click', (e) => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+};
+
+const comInit = () => {
+    navi();
+    topBar();
+    siteMap();
+};
 
 (() => {
     preventDefaultAnchor();
--- js/main.js
+++ js/main.js
@@ -3,10 +3,165 @@ const mainBanner = () => {};
 //이벤트배너
 const eventBanner = () => {};
 
+const sectionPage1 = () => {
+    const $con0 = get('#mainVisual');
+    const $con1 = get('.main .con1');
+    const $con2 = get('.main .con2');
+    const $con3 = get('.main .con3');
+    const $con4 = get('.main .con4');
+    const $menulis = getAll('.menu li');
+    // console.log($con3.offsetTop);
+    // ㄴ con별로 top 위치값 확인
+    let ty = 0;
+
+    $menulis[0].addEventListener('click', (e) => {
+        ty = $con0.offsetTop;
+        window.scrollTo({ top: ty, behavior: 'smooth' });
+    });
+    $menulis[1].addEventListener('click', (e) => {
+        ty = $con1.offsetTop;
+        window.scrollTo({ top: ty, behavior: 'smooth' });
+    });
+    $menulis[2].addEventListener('click', (e) => {
+        ty = $con2.offsetTop;
+        window.scrollTo({ top: ty, behavior: 'smooth' });
+    });
+    $menulis[3].addEventListener('click', (e) => {
+        ty = $con3.offsetTop;
+        window.scrollTo({ top: ty, behavior: 'smooth' });
+    });
+    $menulis[4].addEventListener('click', (e) => {
+        ty = $con4.offsetTop;
+        window.scrollTo({ top: ty, behavior: 'smooth' });
+    });
+};
+
+const sectionPage2 = () => {
+    const $con0 = get('#mainVisual');
+    const $cons = getAll('.main .con');
+    // console.log($cons[0].offsetTop);
+
+    const posY = [];
+    posY.push($con0.offsetTop);
+    $cons.forEach((item) => {
+        posY.push(item.offsetTop);
+    });
+    // console.log(posY);
+    // ㄴ posY 배열을 만들어서 각각의 con별 탑위치 넣어줌
+
+    const $menulis = getAll('.menu li');
+    // console.log($con3.offsetTop);
+    // ㄴ con별로 top 위치값 확인
+
+    $menulis.forEach((itemLi, idx) => {
+        itemLi.addEventListener('click', (e) => {
+            window.scrollTo({ top: posY[idx], behavior: 'smooth' });
+            $menulis.forEach((item) => item.classList.remove('on'));
+            itemLi.classList.add('on');
+            // e.currentTarget.classList.add('on');
+        });
+    });
+};
+
+const menuBar1 = () => {
+    const $menu = get('.menu');
+    const $menulis = getAll('.menu li');
+    const $con0 = get('#mainVisual');
+    const $cons = getAll('.main .con');
+
+    const posY = [];
+    posY.push($con0.offsetTop);
+    $cons.forEach((item) => {
+        posY.push(item.offsetTop);
+    });
+
+    let ty = 0;
+
+    $menulis.forEach((itemLi, idx) => {
+        itemLi.addEventListener('click', (e) => {
+            window.scrollTo({ top: posY[idx], behavior: 'smooth' });
+            $menulis.forEach((item) => item.classList.remove('on'));
+            itemLi.classList.add('on');
+        });
+    });
+
+    window.addEventListener('scroll', (e) => {
+        ty = window.scrollY;
+
+        if (ty >= posY[0]) {
+            $menulis.forEach((item) => item.classList.remove('on'));
+            $menulis[0].classList.add('on');
+        }
+        if (ty >= posY[1]) {
+            $menulis.forEach((item) => item.classList.remove('on'));
+            $menulis[1].classList.add('on');
+        }
+        if (ty >= posY[2]) {
+            $menulis.forEach((item) => item.classList.remove('on'));
+            $menulis[2].classList.add('on');
+        }
+        if (ty >= posY[3]) {
+            $menulis.forEach((item) => item.classList.remove('on'));
+            $menulis[3].classList.add('on');
+        }
+        if (ty >= posY[4]) {
+            $menulis.forEach((item) => item.classList.remove('on'));
+            $menulis[4].classList.add('on');
+        }
+
+        if (ty > 600) {
+            $menu.classList.add('on');
+        } else {
+            $menu.classList.remove('on');
+        }
+    });
+};
+const menuBar = () => {
+    const $menu = get('.menu');
+    const $menulis = getAll('.menu li');
+    const $con0 = get('#mainVisual');
+    const $cons = getAll('.main .con');
+
+    const posY = [];
+    posY.push($con0.offsetTop);
+    $cons.forEach((item) => {
+        posY.push(item.offsetTop);
+    });
+
+    let ty = 0;
+
+    $menulis.forEach((itemLi, idx) => {
+        itemLi.addEventListener('click', (e) => {
+            window.scrollTo({ top: posY[idx], behavior: 'smooth' });
+            // $menulis.forEach((item) => item.classList.remove('on'));
+            // itemLi.classList.add('on');
+            // ㄴ 아래 효과랑 겹쳐서 툭툭 튕긴다??@@
+        });
+    });
+
+    window.addEventListener('scroll', (e) => {
+        ty = window.scrollY;
+        for (let i = 0; i < $menulis.length; i++) {
+            if (ty >= posY[i]) {
+                $menulis.forEach((item) => item.classList.remove('on'));
+                $menulis[i].classList.add('on');
+            }
+        }
+
+        if (ty > 600) {
+            $menu.classList.add('on');
+        } else {
+            $menu.classList.remove('on');
+        }
+    });
+};
+
 //합치기
 const mainInit = () => {
     mainBanner();
     eventBanner();
+    menuBar();
+    // sectionPage();
 };
 
 (() => {
